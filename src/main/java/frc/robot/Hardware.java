159ce79bf9154a6e0d57f1b3129b0d3e6e1041ad
Ingi Helgason <ingib0803@gmail.com>
2019-10-10 17:11:25 -0700

    Add Lesson Changes 10/10 Meeting

---
+++ src/main/java/frc/robot/Hardware.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Hardware {
+    public TalonSRX intakeMotor;
+
+    public Hardware() {
+        intakeMotor = new TalonSRX(4);
+         
+    }
+}
