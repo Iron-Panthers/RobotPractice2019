bf8a3895e609f9b7340b74be81cd49d04b33064c
Jay gupta <jaygupt1973@gmail.com>
2019-10-22 17:04:35 -0700

    A Bunch of Updates
    Updates, Updates, Updates!

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -2,11 +2,14 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import frc.robot.util.SparkMaxMotorGroup;
+
 public class Hardware {
     public TalonSRX intakeMotor;
+    
+    public SparkMaxMotorGroup climbMotors; 
 
     public Hardware() {
         intakeMotor = new TalonSRX(4);
-         
     }
 }
