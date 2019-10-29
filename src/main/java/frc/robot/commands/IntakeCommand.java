b4e2430532ba8147f9ae9f67346026f0cdb6775c
Angelina <asungelina@gmail.com>
2019-10-29 16:03:33 -0700

    Drive SUbsystem
    ya

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -9,6 +9,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
+import frc.robot.util.Constants;
 
 public class IntakeCommand extends Command {
   public IntakeCommand() {
@@ -25,15 +26,13 @@ public class IntakeCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.intake.intake(0.7);
+    Robot.intake.intake(Constants.INTAKE_POWER);
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
     return false;
-    // letter
-    // this is the new comment
   }
 
   // Called once after isFinished returns true
