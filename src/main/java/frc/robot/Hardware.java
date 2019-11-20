6054f769195d6133c6f40c10ffc7275d481ee6fc
sarah min <sarahmin2005@gmail.com>
2019-11-19 17:30:56 -0800

    Added to DriveSubsystem.java
    i think

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -9,9 +9,9 @@ import frc.robot.util.Constants;
 import frc.robot.util.SparkMaxMotorGroup;
 
 public class Hardware {
-    public static final SparkMaxMotorGroup leftDriveMotors = null;
-	public static final SparkMaxMotorGroup rightDriveMotors = null;
-	public static final Solenoid gearShift = null;
+    public final SparkMaxMotorGroup leftDriveMotors = null;
+	public final SparkMaxMotorGroup rightDriveMotors = null;
+	public final Solenoid gearShift = null;
 	public TalonSRX intakeMotor;
     public CANSparkMax leftLeader;
     public CANSparkMax rightLeader;
@@ -20,24 +20,10 @@ public class Hardware {
 
 
     public Hardware() {
-<<<<<<< HEAD
         intakeMotor = new TalonSRX(Constants.Drivebase.INTAKE_MOTOR_PORT);
         leftLeader = new CANSparkMax(Constants.Drivebase.LEFT_LEADER_MOTOR_PORT, MotorType.kBrushless);
         rightLeader = new CANSparkMax(Constants.Drivebase.RIGHT_LEADER_MOTOR_PORT, MotorType.kBrushless);
         leftFollower = new CANSparkMax(Constants.Drivebase.LEFT_FOLLOWER_MOTOR_PORT, MotorType.kBrushless);
         rightFollower = new CANSparkMax(Constants.Drivebase.RIGHT_FOLLOWER_MOTOR_PORT , MotorType.kBrushless);
-=======
-        /* Drivebase motor controller creation */
-        leftLeader = new CANSparkMax(0, MotorType.kBrushless); 
-        leftFollower = new CANSparkMax(1, MotorType.kBrushless); 
-        rightLeader = new CANSparkMax(2, MotorType.kBrushless); 
-        rightFollower = new CANSparkMax(3, MotorType.kBrushless); 
-        
-        gearShift = new Solenoid(Constants.Drivebase.GEAR_SHIFT_PORT); 
-    
-        /* Drivebase configuration */
-        rightDriveMotors = new SparkMaxMotorGroup("Right Drive Motor Group", rightLeader, rightFollower); 
-        leftDriveMotors = new SparkMaxMotorGroup("Left Drive Motor Group", leftLeader, leftFollower); 
->>>>>>> DriveBase
     }
 }
