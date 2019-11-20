9c852bcb3ad736dc8a07d5df4d2479f0aa8873da
sarah min <sarahmin2005@gmail.com>
2019-11-19 17:13:20 -0800

    Added DriveSubsystem.java
    honestly i don't know what I'm doing

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -1,7 +1,6 @@
-package frc.robot; 
+package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.sensors.PigeonIMU;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -10,29 +9,21 @@ import frc.robot.util.Constants;
 import frc.robot.util.SparkMaxMotorGroup;
 
 public class Hardware {
-    /* Drivebase motor controllers */
-    public CANSparkMax leftLeader; 
-    public CANSparkMax leftFollower; 
-    public CANSparkMax rightLeader; 
-    public CANSparkMax rightFollower; 
+    public static final SparkMaxMotorGroup leftDriveMotors = null;
+	public static final SparkMaxMotorGroup rightDriveMotors = null;
+	public static final Solenoid gearShift = null;
+	public TalonSRX intakeMotor;
+    public CANSparkMax leftLeader;
+    public CANSparkMax rightLeader;
+    public CANSparkMax leftFollower;
+    public CANSparkMax rightFollower;
 
-    public TalonSRX gyroTestMotor; 
-    public PigeonIMU gyro; 
-
-    /* Drivebase MotorGroups */
-    public SparkMaxMotorGroup leftDriveMotors; 
-    public SparkMaxMotorGroup rightDriveMotors; 
-
-    /* Climb motor controllers and pneumatics */
-    public Solenoid gearShift; 
 
     public Hardware() {
-        /* Drivebase motor controller creation */
-        leftLeader = new CANSparkMax(0, MotorType.kBrushless); 
-        leftFollower = new CANSparkMax(1, MotorType.kBrushless); 
-        rightLeader = new CANSparkMax(2, MotorType.kBrushless); 
-        rightFollower = new CANSparkMax(3, MotorType.kBrushless); 
-        
-        gearShift = new Solenoid(Constants.Drivebase.GEAR_SHIFT_PORT); 
+        intakeMotor = new TalonSRX(Constants.Drivebase.INTAKE_MOTOR_PORT);
+        leftLeader = new CANSparkMax(Constants.Drivebase.LEFT_LEADER_MOTOR_PORT, MotorType.kBrushless);
+        rightLeader = new CANSparkMax(Constants.Drivebase.RIGHT_LEADER_MOTOR_PORT, MotorType.kBrushless);
+        leftFollower = new CANSparkMax(Constants.Drivebase.LEFT_FOLLOWER_MOTOR_PORT, MotorType.kBrushless);
+        rightFollower = new CANSparkMax(Constants.Drivebase.RIGHT_FOLLOWER_MOTOR_PORT , MotorType.kBrushless);
     }
 }
--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -3,6 +3,18 @@ package frc.robot.util;
 public class Constants {
     public class Drivebase {
         /* DRIVEBASE PORTS */
-        public static final int GEAR_SHIFT_PORT = 6; 
+        public static final int GEAR_SHIFT_PORT = 2;
+		public static final int INTAKE_MOTOR_PORT = 7;
+	public static final int LEFT_LEADER_MOTOR_PORT = 1;
+	public static final int RIGHT_LEADER_MOTOR_PORT = 14;
+	public static final int RIGHT_FOLLOWER_MOTOR_PORT = 15;
+    public static final int LEFT_FOLLOWER_MOTOR_PORT = 0;
+    
+    public static final boolean IS_LEFT_INVERTED = true;
+    public static final boolean IS_RIGHT_INVERTED = true; 
     }
+
+    public static final double LEFT_MOTOR_POWER = 0.3;
+    public static final double RIGHT_MOTOR_POWER = 0.3;
+
 }
