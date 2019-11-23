67399f1906cbde610a9529fe8417d9cd8a6e249d
Chloe <chloeyan365@gmail.com>
2019-11-22 16:14:26 -0800

    Fixed ports

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,7 @@
 package frc.robot;
 
 public class Constants {
-    public static final int JOYSTICK_PORT = 2; //joystick for intake
+    public static final int JOYSTICK_PORT = 1; //or 2 joystick for intake
     public static final int JOYSTICK_INTAKE_BUTTON_PORT = 2; //side button, intake
     public static final int JOYSTICK_OUTTAKE_BUTTON_PORT = 3; //top close left, outtake
     public static final int JOYSTICK_INTAKE_HATCH_PORT = 6; //top far right, intakes hatch
